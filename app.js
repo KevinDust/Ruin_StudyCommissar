c29560a4ab259ce9116dfbb02ef5010348f42986
unknown <1149536116@qq.com>
2016-09-23 18:07:21 +0800

    项目好多东西都好旧啊

--- app.js
+++ app.js
@@ -8,37 +8,16 @@ var http = require('http');
 var path = require('path');
 var partials = require("express-partials");
 var app = express();
-var MongoStore = require('connect-mongo')(express);
-var settings = require('./settings');
-var flash = require('connect-flash');
 var routes = require('./routes');
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser());
-app.use(express.session({
-	secret: settings.cookieSecret,
-	store: new MongoStore({
-		db: settings.db
-	})
-}));
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(partials());
-app.use(flash());
 //app.use(express.bodyParser());
-app.use(express.bodyParser({uploadDir:'./tempFileUploads'}));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
 
 //routes
 routes(app);
